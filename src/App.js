98c63f45c50713700a3a259676c3e161df69bebb
Hamid Aghdaee <hamid.aghdaee@galvanize.com>
2017-07-31 11:31:36 -0700

    newest react-inbox-libs

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import _ from 'lodash';
 import ReactGridLayout from 'react-grid-layout';
 import './App.css';
-import Root from  '@databraid/react-inbox-libs/lib/Root';
+import Root from  '@databraid/react-inbox-libs/lib';
 
 console.log(Root)
 var MyFirstGrid = React.createClass({
